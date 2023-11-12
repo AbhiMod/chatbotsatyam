e35d4a077dbba799fc33d5e27cdbb48b1038fe6b
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-12 07:45:14 +0530

    Create dam.py

---
+++ dam.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && rm -rf chatbotsatyam && git clone https://github.com/AbhiMod/chatbotsatyam && cd chatbotsatyam && python3 db.py")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
