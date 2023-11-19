f7b1d74eba57cc715a999450726354292d0b09be
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-19 11:12:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from datetime import datetime
 
 API_ID = "25450075"
 API_HASH = "278e22b00d6dd565c837405eda49e6f2"
-BOT_TOKEN = os.environ.get("BOT_TOKEN","6684745377:AAFT0sUF1QlIyBlR09ekxSggdon9FxFcT_A") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN","6684745377:AAEYXB3zUT-VegLlG1rqKnUdJukAPaXFUnk") 
 MONGO_URL = "mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = os.environ.get("DATABASE_NAME","VickDb") 
 BOT_USERNAME = os.environ.get("BOT_USERNAME","Tisha_machine_bot")
